a79369b0878e60dee9dea470e10eb8c9e7279821
I-E-Leibowitz <joniwaly@gmail.com>
2022-05-07 15:26:03 +0300

    Added: * Login.kt * Signup.kt
    Fix:
    * functional menu buttons
    * fields staying full when leaving page

--- src/main/kotlin/com/example/view/Login.kt
+++ src/main/kotlin/com/example/view/Login.kt
@@ -4,18 +4,17 @@ import com.example.Styles
 import javafx.beans.InvalidationListener
 import javafx.beans.property.SimpleStringProperty
 import javafx.geometry.Pos
-import javafx.scene.Parent
 import tornadofx.*
 
 class Login : View("Login") {
     override val root = borderpane {
         center = form {
-            val input = SimpleStringProperty()
+            val usrId = SimpleStringProperty()
             val password = SimpleStringProperty()
             hbox(alignment=Pos.CENTER) {
                 fieldset("Login") {
                     addClass(Styles.headingMain)
-                    textfield(input) {
+                    textfield(usrId) {
                         val usrLabel = label("Username/Email")
                         this@textfield.textProperty().addListener(
                             InvalidationListener {
@@ -23,13 +22,19 @@ class Login : View("Login") {
                             }
                         )
                     }
-                    passwordfield {
+                    passwordfield(password) {
                         val passwdLabel = label("Password")
                         this@passwordfield.textProperty().addListener(
                             InvalidationListener {
                                 passwdLabel.setText("")
                             }
                         )
+                    }
+                    button("Log in") {
+                        addClass(Styles.secondaryButton)
+                        action {
+                            println("LOGIN")
+                        }
                     }
                 }
             }
--- src/main/kotlin/com/example/view/Signup.kt
+++ src/main/kotlin/com/example/view/Signup.kt
@@ -1,17 +1,55 @@
 package com.example.view
 
 import com.example.Styles
+import javafx.beans.InvalidationListener
+import javafx.beans.property.SimpleStringProperty
 import javafx.scene.Parent
 import tornadofx.*
 import javafx.geometry.Pos
 
 class Signup : View("Register") {
-    override val root = borderpane {
+    private var username = SimpleStringProperty()
+    private var email = SimpleStringProperty()
+    private var password = SimpleStringProperty()
+    private var passwordVerify = SimpleStringProperty()
+    override var root = borderpane {
         center = form {
             fieldset {
                 hbox(alignment=Pos.CENTER) {
-                    label("Test") {
+                    fieldset("Sign Up") {
                         addClass(Styles.headingMain)
+                        textfield(username) {
+                            val assistLabel = label("username")
+                            this@textfield.textProperty().addListener(
+                                InvalidationListener {
+                                    assistLabel.setText("")
+                                }
+                            )
+                        }
+                        textfield(email) {
+                            val assistLabel = label("email")
+                            this@textfield.textProperty().addListener(
+                                InvalidationListener {
+                                    assistLabel.setText("")
+                                }
+                            )
+                        }
+                        passwordfield(password) {
+                            val assistLabel = label("password")
+                            this@passwordfield.textProperty().addListener(
+                                InvalidationListener {
+                                    assistLabel.setText("")
+                                }
+                            )
+                        }
+                        passwordfield(passwordVerify) {
+                            val assistLabel = label("verify password")
+                            this@passwordfield.textProperty().addListener(
+                                InvalidationListener {
+                                    assistLabel.setText("")
+                                }
+                            )
+                        }
                     }
                 }
             }
@@ -21,6 +59,10 @@ class Signup : View("Register") {
             addClass(Styles.exitButton)
             action {
                 println("BACK")
+                username.value = ""
+                email.value = ""
+                password.value = ""
+                passwordVerify.value = ""
                 this@Signup.replaceWith(MainView::class)
             }
         }
